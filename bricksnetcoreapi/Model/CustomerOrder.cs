25e4eec103879faa8ef079f3c2539de2a7694d97
Lahuraje <lahurajek@gmail.com>
2022-11-26 22:43:54 +0530

    model creation

--- bricksnetcoreapi/Model/CustomerOrder.cs
+++ bricksnetcoreapi/Model/CustomerOrder.cs
@@ -2,5 +2,15 @@
 {
     public class CustomerOrder
     {
+        public int Id { get; set; }
+        public string? OrderId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? MobileNo { get; set; }
+        public string? Address { get; set; }
+        public int? Qty { get; set; }
+        public int? Rate { get; set; }
+        public int? Total { get; set; }
+        public string? Status { get; set; }
+
     }
 }
